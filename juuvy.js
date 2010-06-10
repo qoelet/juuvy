c918d5973ba0a04d64b43eb6eb559a1782b2b238
Kenny Shen Renhui <kenny@northpole.sg>
2010-06-11 02:35:24 +0800

    Reworked juuvy (less other code), and updated example.

--- juuvy.js
+++ juuvy.js
@@ -5,39 +5,37 @@
  */
  
 (function(window) {
-    function Juuvy(keys, jsonobj){
-        this.keys = keys;
-        this.jsonobj = jsonobj;
+    function Juuvy(key, val, font, keyfontsize, valfontsize, orbcol, orbkeycol, orbvalcol, paper){
+        this.key = key;
+        this.val = val;
+        this.font = font;
+        this.orbcol = orbcol;
+        this.orbkeycol = orbkeycol;
+        this.orbvalcol= orbvalcol;
+        this.keyfontstr = keyfontsize + " " + font;
+        this.valfontstr = valfontsize + " " + font;
+        this.paper = paper;
     };
     
     Juuvy.prototype.juuv_it = function() {
-        var numkeys = this.keys.length;
-        //for (i in this.keys) {
-        //    alert(this.jsonobj[this.keys[i]]);
-        //}
-        var paper = Raphael(50, 50, 800, 650);
-        var layer = paper.image("../media/bg.gif", 0,0,800,650);
-        var c = new Array();
-        var keytxt = new Array();
-        var valtxt = new Array();
-
-        for(i in this.keys) {
-            var x = Math.floor(Math.random()*700);
-            var y = Math.floor(Math.random()*450);
+        var x = Math.floor(Math.random()*700);
+        var y = Math.floor(Math.random()*450);
+        
+        x = x + 50;
+        y = y + 50;
+        
+        this.orb = this.paper.circle(x, y, 50);
+        this.orb.attr({fill:this.orbcol,stroke:this.orbcol});
+        var path_data = "M400 300L" + x + " " + y;
+        this.path = this.paper.path(path_data).attr({stroke:this.orbcol,opacity:0.5});
+        
+        // plot orbs
+        this.orbkey = this.paper.text(x,y, this.key).attr({font:this.keyfontstr, fill:this.orbkeycol});
+        if ((typeof(this.val) == "string") || (typeof(this.val) == "number"))
+            this.orbval = this.paper.text(x,y-15,this.val).attr({font:this.valfontstr, fill:this.orbvalcol});
+        else
+            this.orbval = this.paper.text(x,y-15,"{obj/array}").attr({font:this.valfontstr, fill:this.orbvalcol});
             
-            x = x + 50;
-            y = y + 50;
-            
-            c[this.keys[i]] = paper.circle(x, y, 50);
-            c[this.keys[i]].attr({fill:"white",stroke:"white"});
-            var path_data = "M400 300L" + x + " " + y;
-            paper.path(path_data).attr({stroke:"white",opacity:0.5});
-            
-            keytxt[i] = paper.text(x,y, this.keys[i]).attr({font:"10px Georgia", fill:"grey"});
-            valtxt[i] = paper.text(x,y-15,this.jsonobj[this.keys[i]]).attr({font:"14px Georgia", fill:"red"});
-        }
-    };
-    
-    
+    };    
     window.Juuvy = Juuvy;
 })(window)

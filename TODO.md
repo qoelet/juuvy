6a68b64660a94ece39fe3cb04e4be6e99ce5358f
Kenny Shen Renhui <kenny@northpole.sg>
2010-06-11 17:51:59 +0800

    Added fading for empty length vals, experimenting with text-fitting inside orb, added a couchdb json sample to example

--- TODO.md
+++ TODO.md
@@ -1,5 +1,18 @@
 # wishlist for Juuvy #
 
+* 0.1 todo *
+
+1. Ability to get full key/val on hover - DONE
+
+2. Faded orbs for empty vals - DONE
+
+3. Text should nest inside orb neatly.
+
+4. Toggle mode a)equal b)size-weighted
+
+	-- equal: all orbs are same size
+	-- size-weighted: orbsize is relative to val length
+
 * 0.2 wishlist *
 
 1. nested json - e.g. arrays should be branched out further from parent orb
--- juuvy.js
+++ juuvy.js
@@ -18,29 +18,41 @@
         this.orbvalcol= orbvalcol;
         this.keyfontstr = keyfontsize + " " + font;
         this.valfontstr = valfontsize + " " + font;
+        this.valfontsize = valfontsize;
+        this.keyfontsize = keyfontsize;
         
         // drawing setup
         this.paper = paper;
+        this.opc = 1;
         this.x = Math.floor(Math.random()*700) + 50;
         this.y = Math.floor(Math.random()*450) + 50;
         
         // info bar
         this.mykeytxt = paper.text(400,615,key).attr({font:this.keyfontstr,fill:"grey"});
-        this.myvaltxt = paper.text(400,635,val).attr({font:this.keyfontstr,fill:"grey"});
+        this.myvaltxt = paper.text(400,635,val.toString()).attr({font:this.keyfontstr,fill:"grey"});
 		this.mykeytxt.hide();
 		this.myvaltxt.hide();
     };
     
     Juuvy.prototype.juuv_it = function() {
         // plot orbs
+        if(this.val.toString().replace(/\s+/g,'').length > 0) {}
+        else {this.opc = 0.4}
         this.orb = this.paper.circle(this.x, this.y, this.orbsize);
-        this.orb.attr({fill:this.orbcol,stroke:this.orbcol});
+        this.orb.attr({fill:this.orbcol,stroke:this.orbcol,opacity:this.opc});
         var path_data = "M400 300L" + this.x + " " + this.y;
         this.path = this.paper.path(path_data).attr({stroke:this.orbcol,opacity:0.5});
         
         // plot orb text
-        if ((typeof(this.val) == "string") || (typeof(this.val) == "number"))
-            this.orbval = this.paper.text(this.x,this.y,this.val).attr({font:this.valfontstr, fill:this.orbvalcol});
+        if ((typeof(this.val) == "string") || (typeof(this.val) == "number")) {
+            if((this.val.toString().length * parseInt(this.valfontsize.slice(0,2))) > (this.orbsize*2) ) {
+                var shortenedval = this.val.toString().slice(0, parseInt(this.valfontsize.slice(0,2))-5) + '...';
+                this.orbval = this.paper.text(this.x,this.y,shortenedval).attr({font:this.valfontstr, fill:this.orbvalcol});
+            }
+            else {
+                this.orbval = this.paper.text(this.x,this.y,this.val.toString()).attr({font:this.valfontstr, fill:this.orbvalcol});
+            }
+        }
         else
             this.orbval = this.paper.text(this.x,this.y,"{obj/array}").attr({font:this.valfontstr, fill:this.orbvalcol});
             

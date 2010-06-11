ef1c0cc4c13c4b79697eff866509f714f8f17eed
Kenny Shen Renhui <kenny@northpole.sg>
2010-06-11 17:00:57 +0800

    Extended example to show key/val on hover, updated juuvy with get methods

--- juuvy.js
+++ juuvy.js
@@ -5,37 +5,52 @@
  */
  
 (function(window) {
-    function Juuvy(key, val, font, keyfontsize, valfontsize, orbcol, orbkeycol, orbvalcol, paper){
+    function Juuvy(key, val, font, keyfontsize, valfontsize, orbsize, orbcol, orbkeycol, orbvalcol, paper){
         this.key = key;
         this.val = val;
+        this.orb = {};
+        
+        // user settings
         this.font = font;
         this.orbcol = orbcol;
+        this.orbsize = orbsize;
         this.orbkeycol = orbkeycol;
         this.orbvalcol= orbvalcol;
         this.keyfontstr = keyfontsize + " " + font;
         this.valfontstr = valfontsize + " " + font;
+        
+        // drawing setup
         this.paper = paper;
+        this.x = Math.floor(Math.random()*700) + 50;
+        this.y = Math.floor(Math.random()*450) + 50;
+        
+        // info bar
+        this.mykeytxt = paper.text(400,615,key).attr({font:this.keyfontstr,fill:"grey"});
+        this.myvaltxt = paper.text(400,635,val).attr({font:this.keyfontstr,fill:"grey"});
+		this.mykeytxt.hide();
+		this.myvaltxt.hide();
     };
     
     Juuvy.prototype.juuv_it = function() {
-        var x = Math.floor(Math.random()*700);
-        var y = Math.floor(Math.random()*450);
-        
-        x = x + 50;
-        y = y + 50;
-        
-        this.orb = this.paper.circle(x, y, 50);
+        // plot orbs
+        this.orb = this.paper.circle(this.x, this.y, this.orbsize);
         this.orb.attr({fill:this.orbcol,stroke:this.orbcol});
-        var path_data = "M400 300L" + x + " " + y;
+        var path_data = "M400 300L" + this.x + " " + this.y;
         this.path = this.paper.path(path_data).attr({stroke:this.orbcol,opacity:0.5});
         
-        // plot orbs
-        this.orbkey = this.paper.text(x,y, this.key).attr({font:this.keyfontstr, fill:this.orbkeycol});
+        // plot orb text
         if ((typeof(this.val) == "string") || (typeof(this.val) == "number"))
-            this.orbval = this.paper.text(x,y-15,this.val).attr({font:this.valfontstr, fill:this.orbvalcol});
+            this.orbval = this.paper.text(this.x,this.y,this.val).attr({font:this.valfontstr, fill:this.orbvalcol});
         else
-            this.orbval = this.paper.text(x,y-15,"{obj/array}").attr({font:this.valfontstr, fill:this.orbvalcol});
+            this.orbval = this.paper.text(this.x,this.y,"{obj/array}").attr({font:this.valfontstr, fill:this.orbvalcol});
             
-    };    
+    }; 
+    
+    Juuvy.prototype.give_key = function() {
+        return this.mykeytxt;
+    }
+    Juuvy.prototype.give_val = function() {
+        return this.myvaltxt;
+    }
     window.Juuvy = Juuvy;
 })(window)
